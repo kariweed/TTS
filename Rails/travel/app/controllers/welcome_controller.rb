e00a1d3e0920895ca5c0195ba637afdc540e5feb
Kari Weed <kari.weed@hotmail.com>
2015-05-09 07:43:29 -0400

    udpate

--- Rails/travel/app/controllers/welcome_controller.rb
+++ Rails/travel/app/controllers/welcome_controller.rb
@@ -3,6 +3,10 @@ class WelcomeController < ApplicationController
   	@homeland = "Italy"
   	@countries = ["Germany", "France", "Spain", "Poland", "England", "Canada", "Sweden"]
   	@images = ["italy.jpg", "france.jpg", "spain.jpg", "canada.jpg"]
+  	@cities = ["Paris", "Nice", "Wellington", "Nova_Scotia"]
+  	@country = ["France", "France", "New_Zealand", "Canada"]
+
+  	#@destinations = {"Paris" => "France", "Nice" => "France", "Wellington" => "New Zealand", "Nova Scotia" => "Canada"}
   end
 
 
