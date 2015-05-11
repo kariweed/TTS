5dd905dcbf17d1c75ee7fb3f374c11c42b3ac6d7
Kari Weed <kari.weed@hotmail.com>
2015-05-11 15:14:26 -0400

    Classwork 5/11

--- Rails/travel/app/controllers/welcome_controller.rb
+++ Rails/travel/app/controllers/welcome_controller.rb
@@ -5,6 +5,10 @@ class WelcomeController < ApplicationController
   	@images = ["italy.jpg", "france.jpg", "spain.jpg", "canada.jpg"]
   	@cities = ["Paris", "Nice", "Wellington", "Nova_Scotia"]
   	@country = ["France", "France", "New_Zealand", "Canada"]
+    @united_states = {"capital city" => "Washington, DC", 
+                     "favorite city" => "Asheville, NC", 
+                     "favorite state" => "Oregon", 
+                     "flag colors" => ["red", "white", "blue"]}
 
   	#@destinations = {"Paris" => "France", "Nice" => "France", "Wellington" => "New Zealand", "Nova Scotia" => "Canada"}
   end
@@ -14,4 +18,10 @@ class WelcomeController < ApplicationController
   	@color = params[:color]
   	@size = params[:size].to_i
   end
+
+  def contact
+  end
+
+
+  
 end
